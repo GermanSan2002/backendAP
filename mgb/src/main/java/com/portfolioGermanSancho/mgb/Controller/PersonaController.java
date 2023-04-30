f1478b4fcb6bed1efb8dc4edc7e4caf21d59ac90
GermanSan2002 <germansan2002@outlook.com>
2023-04-30 20:34:11 -0300

    Creacion de personas y login

--- mgb/src/main/java/com/portfolioGermanSancho/mgb/Controller/PersonaController.java
+++ mgb/src/main/java/com/portfolioGermanSancho/mgb/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolioGermanSancho.mgb.Entity.Persona;
 import com.portfolioGermanSancho.mgb.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -45,5 +47,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         
         return "Se ha modificado la persona correctamente";
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona traerPerfil(){
+        return ipersonaService.findPersona((long)1);
     }
 }
