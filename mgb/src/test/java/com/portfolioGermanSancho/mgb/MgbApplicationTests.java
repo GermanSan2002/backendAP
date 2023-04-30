f407947ae96020053f4dffa29c910512185682fe
GermanSan2002 <germansan2002@outlook.com>
2023-04-30 03:45:53 -0300

    Inicio proyecto

---
+++ mgb/src/test/java/com/portfolioGermanSancho/mgb/MgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolioGermanSancho.mgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
