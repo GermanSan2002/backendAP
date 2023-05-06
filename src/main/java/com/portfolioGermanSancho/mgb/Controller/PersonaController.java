1f367cf3279c7448be2c2f2064db5429955c5355
GermanSan2002 <germansan2002@outlook.com>
2023-05-06 17:22:19 -0300

    Crear persona

--- src/main/java/com/portfolioGermanSancho/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolioGermanSancho/mgb/Controller/PersonaController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,12 +50,12 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     }*/
     
-    /*@PostMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody DTOPersona dtoPersona){
-        if(StringUtils.isBlank(dtoPersona.getNombreE())){
+        if(StringUtils.isBlank(dtoPersona.getNombre())){
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
         }
-        if(iPersonaService.existsByNombreE(dtoPersona.getNombreE())){
+        if(iPersonaService.existsByNombre(dtoPersona.getNombre())){
             return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
         }
         
@@ -63,7 +64,7 @@ public class PersonaController {
             );
         iPersonaService.save(persona);
         return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
-    }*/
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody DTOPersona dtopersona){
